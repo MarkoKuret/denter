fc0f727b2056a37be5529485d6881c18e35596cd
bjukic2 <bruno@DESKTOP-D9JD4J8>
2022-12-18 10:52:05 +0100

    Updated register, login

--- denter/rute.py
+++ denter/rute.py
@@ -34,7 +34,7 @@ def prijava():
         if korisnik and check_password_hash(korisnik.lozinka, obrazac.lozinka.data):
             #spremi korisnikov jedinstveni id u session
             session['korisnik_id'] = korisnik.id
-            flash(f'uspjesan login za {obrazac.email.data}', 'dobro')
+            flash(f'uspješan login za {obrazac.email.data}', 'dobro')
             return redirect(url_for('home'))
 
         flash("nevažeći email ili lozinka", 'lose')
@@ -43,25 +43,6 @@ def prijava():
 
 @app.route("/registracija_klijenti", methods=["GET", "POST"])
 def registracija_klijenti():
-    obrazac = RegistracijaObrazacOsoblje()
-
-    if obrazac.validate_on_submit():
-        #generacija hasha
-        lozinka_hash = generate_password_hash(obrazac.lozinka.data)
-        korisnik = Korisnik(ime=obrazac.ime.data, email=obrazac.email.data, lozinka=lozinka_hash)
-        db.session.add(korisnik)
-        db.session.commit()
-
-        #spremanje logina u kolacic
-        session["korisnik_id"] = korisnik.id
-        flash('Bok ' + obrazac.ime.data + ', uspiješno ste registrirani', 'dobro')
-
-        return redirect(url_for('home'))
-
-    return render_template('registracija_klijenti.html', naslov='registracija', obrazac=obrazac)
-
-@app.route("/registracija_osoblje", methods=["GET", "POST"])
-def registracija_osoblje():
     obrazac = RegistracijaObrazacKlijenti()
 
     if obrazac.validate_on_submit():
@@ -73,7 +54,26 @@ def registracija_osoblje():
 
         #spremanje logina u kolacic
         session["korisnik_id"] = korisnik.id
-        flash('Bok ' + obrazac.ime.data + ', uspiješno ste registrirani', 'dobro')
+        flash('Bok ' + obrazac.ime.data + ', uspješno ste registrirani', 'dobro')
+
+        return redirect(url_for('home'))
+
+    return render_template('registracija_klijenti.html', naslov='registracija', obrazac=obrazac)
+
+@app.route("/registracija_osoblje", methods=["GET", "POST"])
+def registracija_osoblje():
+    obrazac = RegistracijaObrazacOsoblje()
+
+    if obrazac.validate_on_submit():
+        #generacija hasha
+        lozinka_hash = generate_password_hash(obrazac.lozinka.data)
+        korisnik = Korisnik(ime=obrazac.ime.data, email=obrazac.email.data, lozinka=lozinka_hash)
+        db.session.add(korisnik)
+        db.session.commit()
+
+        #spremanje logina u kolacic
+        session["korisnik_id"] = korisnik.id
+        flash('Bok ' + obrazac.ime.data + ', uspješno ste registrirani', 'dobro')
 
         return redirect(url_for('home'))
 
